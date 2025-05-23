9c85210fb58298529bc0b233381b3ee597385b71
shewafem <tamirsundureev@gmail.com>
2025-05-23 04:15:09 +0300

    auth route naming

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,9 @@
-//import Image from "next/image";
-
 import Hero from "@/components/pages/hero";
-//import { useSession } from "next-auth/react";
 
 export default function Home() {
 	return (
 		<>
 			<Hero/>
-      {/*<HowItWorks></HowItWorks>*/}
 		</>
 	);
 }
