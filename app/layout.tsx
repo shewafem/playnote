a961ba44d297b857b1a03d6293bdf18f4da8f82c
shewafem <tamirsundureev@gmail.com>
2025-06-02 19:49:06 +0300

    added vercel analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,6 +8,7 @@ import { Container } from "@/components/layout/container";
 import { SessionProvider } from "next-auth/react";
 import { AppBreadcrumbs } from "@/components/layout/breadcrumbs";
 import { Toaster } from "sonner";
+import { Analytics } from "@vercel/analytics/next"
 
 const geologica = Geologica({
 	variable: "--font-geologica",
@@ -47,6 +48,7 @@ export default function RootLayout({
 							<Container>
 								<AppBreadcrumbs />
                 {children}
+								<Analytics />
 							</Container>
 						</main>
             <Toaster richColors/>
